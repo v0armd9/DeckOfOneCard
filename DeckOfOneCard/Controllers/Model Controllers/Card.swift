655889721a3ea27a007a20216df617e216289f16
Darin Armstrong <darin.marmstrong@gmail.com>
2019-06-24 11:15:00 -0600

    Created Model

---
+++ DeckOfOneCard/Controllers/Model Controllers/Card.swift
@@ -0,0 +1,24 @@
+//
+//  Card.swift
+//  DeckOfOneCard
+//
+//  Created by Darin Marcus Armstrong on 6/24/19.
+//  Copyright © 2019 Darin Marcus Armstrong. All rights reserved.
+//
+
+import Foundation
+
+class Card: Codable {
+    let suit: String
+    let value: String
+    let code: String
+    let image: String
+    
+    init(suit: String, value: String, code: String, image: String) {
+    
+        self.suit = suit
+        self.value = value
+        self.code = code
+        self.image = image
+    }
+}
